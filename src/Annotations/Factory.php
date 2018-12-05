09282f86f5b91fbb4497981e6bfe352192692c2b
David Négrier <d.negrier@thecodingmachine.com>
2018-12-05 12:13:05 +0100

    Adding factory annotation

---
+++ src/Annotations/Factory.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace TheCodingMachine\GraphQL\Controllers\Annotations;
+
+/**
+ * Factories are methods used to declare GraphQL input types.
+ *
+ * @Annotation
+ * @Target({"METHOD"})
+ * @Attributes({
+ *   @Attribute("name", type = "string")
+ * })
+ */
+class Factory
+{
+    /**
+     * @var string|null
+     */
+    private $name;
+
+    /**
+     * @param mixed[] $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->name = $attributes['name'] ?? null;
+    }
+
+    /**
+     * Returns the name of the GraphQL input type.
+     * If not specified, the name of the method should be used instead.
+     *
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}
