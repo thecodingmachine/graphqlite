2dfb338ec7f0b5c888288f60eaa2cad16fc6bccc
David Négrier <d.negrier@thecodingmachine.com>
2018-12-21 17:37:17 +0100

    Improving tests

--- tests/Hydrators/FactoryHydratorTest.php
+++ tests/Hydrators/FactoryHydratorTest.php
@@ -4,15 +4,31 @@ namespace TheCodingMachine\GraphQL\Controllers\Hydrators;
 
 use GraphQL\Type\Definition\InputObjectType;
 use PHPUnit\Framework\TestCase;
+use stdClass;
+use TheCodingMachine\GraphQL\Controllers\Types\ResolvableInputObjectType;
 
 class FactoryHydratorTest extends TestCase
 {
     public function testHydratorNotFound()
     {
-        $factoryHydrator = new FactoryHydrator();
-        $this->expectException(CannotHydrateException::class);
-        $factoryHydrator->hydrate([], new InputObjectType([
+        $resolvableInputObjectType = $this->getMockBuilder(ResolvableInputObjectType::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['resolve'])
+            ->getMock();
+        $resolvableInputObjectType->method('resolve')->willReturn(new stdClass());
+
+        $badObjectType = new InputObjectType([
             'name' => 'foo'
-        ]));
+        ]);
+
+        $factoryHydrator = new FactoryHydrator();
+
+        $this->assertTrue($factoryHydrator->canHydrate([], $resolvableInputObjectType));
+        $this->assertFalse($factoryHydrator->canHydrate([], $badObjectType));
+
+        $this->assertEquals(new stdClass(), $factoryHydrator->hydrate([], $resolvableInputObjectType));
+
+        $this->expectException(CannotHydrateException::class);
+        $factoryHydrator->hydrate([], $badObjectType);
     }
 }
