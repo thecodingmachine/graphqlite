23f3f96fa0c04fda6a81eb2c57bb460333743208
David Négrier <d.negrier@thecodingmachine.com>
2019-01-07 14:42:08 +0100

    Improving code coverage

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -25,8 +25,12 @@ class SchemaTest extends AbstractQueryProviderTest
 
         $fields = $schema->getQueryType()->getFields();
         $this->assertArrayHasKey('dummyQuery', $fields);
+        $resolve = $fields['dummyQuery']->resolveFn;
+        $this->assertSame('This is a placeholder query. Please create a query using the @Query annotation.', $resolve());
 
         $fields = $schema->getMutationType()->getFields();
         $this->assertArrayHasKey('dummyMutation', $fields);
+        $resolve = $fields['dummyMutation']->resolveFn;
+        $this->assertSame('This is a placeholder mutation. Please create a mutation using the @Mutation annotation.', $resolve());
     }
 }
