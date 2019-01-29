98aa781a2d95c608ad74f3525e8de882d08bfbca
David Négrier <d.negrier@thecodingmachine.com>
2019-01-29 13:56:54 +0100

    Adding first page + commiting rename message

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,62 +37,36 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('getting-started', this.props.language)}>
+              Install
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl('my-first-query', this.props.language)}>
+              Getting started
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
-          </div>
-          <div>
-            <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
+            <a href={this.docUrl('annotations_reference', this.props.language)}>
+              Reference
             </a>
           </div>
+
           <div>
             <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
+            <a href="https://thecodingmachine.io" target="_blank">Blog</a>
+            <a href="https://github.com/thecodingmachine/graphql-controllers" target="_blank">GitHub</a>
+            <a className="github-button"
+               href="https://github.com/thecodingmachine/graphql-controllers"
+               data-icon="octicon-star"
+               data-show-count="true"
+               aria-label="Star thecodingmachine/graphql-controllers on GitHub">Star</a>
           </div>
         </section>
 
         <a
-          href="https://code.facebook.com/projects/"
+          href="https://thecodingmachine.io/open-source"
           target="_blank"
-          rel="noreferrer noopener"
           className="fbOpenSource">
           <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
+            src={`${this.props.config.baseUrl}img/tcm.png`}
+            alt="Proudly brought to you by TheCodingMachine"
           />
         </a>
         <section className="copyright">{this.props.config.copyright}</section>
--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -29,11 +29,11 @@ class HomeSplash extends React.Component {
       </div>
     );
 
-    const Logo = props => (
+    /*const Logo = props => (
       <div className="projectLogo">
         <img src={props.img_src} alt="Project Logo" />
       </div>
-    );
+    );*/
 
     const ProjectTitle = () => (
       <h2 className="projectTitle">
@@ -60,13 +60,13 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/logo.svg`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
+            <img width={200} src="img/logo.svg" alt="Project Logo" />
           <PromoSection>
             <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('getting-started')}>Install</Button>
+            <Button href="https://github.com/thecodingmachine/graphql-controllers">GitHub</Button>
           </PromoSection>
         </div>
       </SplashContainer>
@@ -95,67 +95,93 @@ class Index extends React.Component {
     const FeatureCallout = () => (
       <div
         className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
+        >
+        <h2 style={{textAlign: 'center'}}>Comes with batteries included</h2>
+          <ul>
+              <li><a href="docs/my-first-query">Queries</a></li>
+              <li><a href="docs/mutations">Mutations</a></li>
+              <li><a href="docs/type_mapping#mapping-of-arrays">Mapping of arrays / iterators</a></li>
+              <li><a href="docs/input-types">Support for input types</a></li>
+              <li><a href="docs/extend_type">Extendable types</a></li>
+              <li>Plugs into your <a href="docs/authentication_authorization">framework security module</a></li>
+              <li>Map <a href="docs/inheritance">PHP inheritance to GraphQL interfaces</a> automatically</li>
+              <li><a href="docs/type_mapping#union-types">Union types</a></li>
+              <li><a href="docs/file-uploads">GraphQL file uploads</a></li>
+              <li><a href="docs/type_mapping#mapping-of-dates">Native DateTime type</a></li>
+          </ul>
       </div>
     );
 
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content: 'Talk about trying this out',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'left',
-            title: 'Try it Out',
-          },
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    );
-
-    const LearnHow = () => (
+    /*const LearnHow = () => (
       <Block background="light">
         {[
           {
-            content: 'Talk about learning how to use this',
+            content: 'Talk about learning how to use this. <pre><code>Foo</code></pre>',
             image: `${baseUrl}img/docusaurus.svg`,
             imageAlign: 'right',
-            title: 'Learn How',
+            title: 'Create a complete GraphQL API using simple annotations',
           },
         ]}
       </Block>
-    );
+    );*/
+
+      const LearnHow = () => (
+          <Container background="light">
+              <GridBlock
+                  layout="twoColumn"
+                  contents={[
+                      {
+                          title: `Use PHP Annotations to declare your GraphQL API`,
+                          content: `Create a complete GraphQL API by simply **annotating** your PHP classes.
+
+You focus on your PHP code.
+
+GraphQL-Controllers comes with an advanced *type-mapper* that automatically translate your PHP types into GraphQL types.`,
+                      },
+                      {
+                          content: '```php\n' +
+                              '/**\n' +
+                              ' * @Type()\n' +
+                              ' */\n' +
+                              'class Product\n' +
+                              '{\n' +
+                              '    /**\n' +
+                              '     * @Field()\n' +
+                              '     */\n' +
+                              '    public function getName(): string\n' +
+                              '    {\n' +
+                              '        return $this->name;\n' +
+                              '    }\n',
+                      },
+                  ]}
+              />
+          </Container>
+      );
 
     const Features = () => (
       <Block layout="fourColumn">
         {[
           {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/docusaurus.svg`,
+            content: 'Almost [no code needed](docs/my-first-query) to switch your API to GraphQL!',
+            image: `${baseUrl}img/at2.svg`,
+            imageLink: "docs/my-first-query",
             imageAlign: 'top',
-            title: 'Feature One',
+            title: 'Annotation based',
           },
           {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/docusaurus.svg`,
+            content: 'Can be [installed in any PHP project](docs/other-frameworks)',
+            image: `${baseUrl}img/php-fig.jpg`,
+            imageLink: "docs/other-frameworks",
             imageAlign: 'top',
-            title: 'Feature Two',
+            title: 'Framework agnostic',
           },
+      {
+          content: 'Native [integration with Symfony provided](docs/symfony-bundle)',
+          image: `${baseUrl}img/symfony_black_03.svg`,
+          imageLink: "docs/symfony-bundle",
+          imageAlign: 'top',
+          title: '... but Symfony bundle available',
+      },
         ]}
       </Block>
     );
@@ -189,16 +215,110 @@ class Index extends React.Component {
       );
     };
 
+
+      const Sample1 = () => (
+          <Container background="light">
+              <GridBlock
+                  layout="twoColumn"
+                  contents={[
+                      {
+                          title: `1. Declare a query in your controller`,
+                          content: `Annotate the query with the @Query annotation`,
+                      },
+                      {
+                          content: `
+\`\`\`php
+class ProductController
+{
+    /**
+     * @Query
+     */
+    public function product(string $id): Product
+    {
+        // Some code that looks for a product and returns it.
+    }
+}
+\`\`\`
+` ,
+                      },
+                  ]}
+              />
+          </Container>
+      );
+
+      const Sample2 = () => (
+          <Container background="">
+              <GridBlock
+                  layout="twoColumn"
+                  contents={[
+                      {
+                          content: `
+\`\`\`php
+/**
+ * @Type()
+ */
+class Product
+{
+    /**
+     * @Field()
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    // ...
+}
+\`\`\`
+` ,
+                      },
+                      {
+                          title: `2. Annotate your types`,
+                          content: "Annotate the `Product` class to declare what fields are exposed to the GraphQL API",
+                      },
+                  ]}
+              />
+          </Container>
+      );
+
+      const Sample3 = () => (
+          <Container background="light">
+              <GridBlock
+                  layout="twoColumn"
+                  contents={[
+                      {
+                          title: `3. Query and enjoy`,
+                          content: `You're good to go! Query and enjoy!`,
+                      },
+                      {
+                          content: `
+\`\`\`graphql
+{
+  product(id: 42) {
+    name
+  }
+}
+\`\`\`
+` ,
+                      },
+                  ]}
+              />
+          </Container>
+      );
+
     return (
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
+          <LearnHow />
           <Features />
           <FeatureCallout />
-          <LearnHow />
-          <TryOut />
-          <Description />
-          <Showcase />
+            <Container background="dark">
+                <h2 style={{textAlign: 'center'}}>Get started!</h2>
+            </Container>
+          <Sample1 />
+          <Sample2 />
+          <Sample3 />
+            <Showcase/>
         </div>
       </div>
     );
