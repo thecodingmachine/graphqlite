9c22c94416c9b47970ac9b2d953b74864ae4a804
David Négrier <d.negrier@thecodingmachine.com>
2019-04-24 17:35:05 +0200

    Adding unit tests to improve code coverage

---
+++ tests/Mappers/Root/CompositeRootTypeMapperTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TheCodingMachine\GraphQLite\Mappers\Root;
+
+use GraphQL\Type\Definition\InputType;
+use GraphQL\Type\Definition\OutputType;
+use phpDocumentor\Reflection\DocBlock;
+use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Integer;
+use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
+
+class CompositeRootTypeMapperTest extends TestCase
+{
+    private function getNullTypeMapper(): RootTypeMapperInterface
+    {
+        return new class() implements RootTypeMapperInterface {
+            public function toGraphQLOutputType(Type $type, ?OutputType $subType, ReflectionMethod $refMethod, DocBlock $docBlockObj): ?OutputType
+            {
+                return null;
+            }
+
+            public function toGraphQLInputType(Type $type, ?InputType $subType, string $argumentName, ReflectionMethod $refMethod, DocBlock $docBlockObj): ?InputType
+            {
+                return null;
+            }
+        };
+    }
+
+    public function testToGraphQLInputType()
+    {
+        $typeMapper = new CompositeRootTypeMapper([$this->getNullTypeMapper()]);
+        $this->assertNull($typeMapper->toGraphQLOutputType(new Integer(), null, new ReflectionMethod(CompositeRootTypeMapper::class, '__construct'), new DocBlock()));
+    }
+
+    public function testToGraphQLOutputType()
+    {
+        $typeMapper = new CompositeRootTypeMapper([$this->getNullTypeMapper()]);
+        $this->assertNull($typeMapper->toGraphQLInputType(new Integer(), null, 'foo', new ReflectionMethod(CompositeRootTypeMapper::class, '__construct'), new DocBlock()));
+    }
+}
