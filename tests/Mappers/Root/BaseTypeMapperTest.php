1d55896eb61f1f225216067a2c91b71913ded780
David Négrier <d.negrier@thecodingmachine.com>
2019-06-06 21:08:57 +0200

    Improving code coverage

--- tests/Mappers/Root/BaseTypeMapperTest.php
+++ tests/Mappers/Root/BaseTypeMapperTest.php
@@ -4,8 +4,10 @@ namespace TheCodingMachine\GraphQLite\Mappers\Root;
 
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\Fqsen;
+use phpDocumentor\Reflection\Types\Array_;
 use phpDocumentor\Reflection\Types\Nullable;
 use phpDocumentor\Reflection\Types\Object_;
+use phpDocumentor\Reflection\Types\Resource_;
 use ReflectionMethod;
 use TheCodingMachine\GraphQLite\AbstractQueryProviderTest;
 use TheCodingMachine\GraphQLite\GraphQLException;
@@ -29,4 +31,16 @@ class BaseTypeMapperTest extends AbstractQueryProviderTest
         $this->expectExceptionMessage('Type-hinting a parameter against DateTime is not allowed. Please use the DateTimeImmutable type instead.');
         $baseTypeMapper->toGraphQLInputType(new Object_(new Fqsen('\\DateTime')), null, 'foo', new ReflectionMethod(BaseTypeMapper::class, '__construct'), new DocBlock());
     }
+
+    public function testUnmappableArray()
+    {
+        $baseTypeMapper = new BaseTypeMapper($this->getTypeMapper());
+
+        $mappedType = $baseTypeMapper->toGraphQLOutputType(new Array_(new Resource_()), null, new ReflectionMethod(BaseTypeMapper::class, '__construct'), new DocBlock());
+        $this->assertNull($mappedType);
+
+        $mappedType = $baseTypeMapper->toGraphQLInputType(new Array_(new Resource_()), null, 'foo', new ReflectionMethod(BaseTypeMapper::class, '__construct'), new DocBlock());
+        $this->assertNull($mappedType);
+    }
+
 }
