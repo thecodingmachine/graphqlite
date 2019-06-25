03f9ea2c0f8ac3edc6cc3d8142b3a26d195c9d57
David Négrier <d.negrier@thecodingmachine.com>
2019-06-25 10:25:50 +0200

    Improving code coverage

--- tests/Annotations/ParameterTest.php
+++ tests/Annotations/ParameterTest.php
@@ -14,4 +14,33 @@ class ParameterTest extends TestCase
         $this->expectExceptionMessage('The @Parameter annotation must be passed a target. For instance: "@Parameter(for="$input", inputType="MyInputType")"');
         new Parameter(['annotations'=>[]]);
     }
+
+    public function testGetAnnotationByTypeException()
+    {
+        $parameter = new Parameter([
+            'for' => 'foo',
+            'annotations' => [
+                new Autowire([]),
+                new Autowire([])
+            ]
+        ]);
+
+        $this->expectException(TooManyAnnotationsException::class);
+        $parameter->getAnnotationByType(Autowire::class);
+    }
+
+    public function testGetAllAnnotations()
+    {
+        $annotations = [
+            new Autowire([]),
+            new Autowire([])
+        ];
+
+        $parameter = new Parameter([
+            'for' => 'foo',
+            'annotations' => $annotations
+        ]);
+
+        $this->assertSame($annotations, $parameter->getAllAnnotations());
+    }
 }
