6c44f933f741827e2be98acb350d6f6998ec7455
David Négrier <d.negrier@thecodingmachine.com>
2019-07-03 16:46:23 +0200

    Adding a failing test case

---
+++ tests/Fixtures/InheritedInputTypes/ChildTestFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace TheCodingMachine\GraphQLite\Fixtures\InheritedInputTypes;
+
+use DateTimeInterface;
+use function implode;
+use TheCodingMachine\GraphQLite\Annotations\Factory;
+use TheCodingMachine\GraphQLite\Fixtures\TestObject;
+use TheCodingMachine\GraphQLite\Fixtures\TestObject2;
+use TheCodingMachine\GraphQLite\Fixtures\TestObjectWithRecursiveList;
+
+class ChildTestFactory extends AbstractTestFactory
+{
+
+}
