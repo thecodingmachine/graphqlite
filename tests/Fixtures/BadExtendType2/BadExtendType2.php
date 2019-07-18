4cc4770681325d661bd88e7333789c82ce67b8e6
David Négrier <d.negrier@thecodingmachine.com>
2019-07-18 14:51:31 +0200

    Improving test coverage

---
+++ tests/Fixtures/BadExtendType2/BadExtendType2.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace TheCodingMachine\GraphQLite\Fixtures\BadExtendType2;
+
+use TheCodingMachine\GraphQLite\Annotations\ExtendType;
+
+/**
+ * @ExtendType(class=\Exception::class)
+ */
+class BadExtendType2
+{
+}
