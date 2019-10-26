e41c375c9d8f6369ce7575ed074bf596360a15e9
Alexey Belyakovskiy <2697024@gmail.com>
2019-10-26 12:54:20 +0300

    Pass link to source into field resolver

--- src/QueryField.php
+++ src/QueryField.php
@@ -256,6 +256,7 @@ class QueryField extends FieldDefinition
                 $exceptions[] = $e;
             }
         }
+        $toPassArgs[] = $source;
         GraphQLAggregateException::throwExceptions($exceptions);
 
         return $toPassArgs;
