e5d429a59aa2491d6277e0739f5f633344437e1b
David Négrier <d.negrier@thecodingmachine.com>
2020-12-07 15:11:27 +0100

    Fixing CI tests of dependencies with Composer 2

--- tests/dependencies/makeComposerLocal.php
+++ tests/dependencies/makeComposerLocal.php
@@ -6,8 +6,6 @@ $composerBundlePath = $argv[1];
 //fetch the dev-master alias from the local graphqlite composer
 $composerGraphqlite = json_decode(file_get_contents(__DIR__.'/copy/composer.json'), true);
 
-$masterAlias = $composerGraphqlite['extra']['branch-alias']['dev-master'];
-
 //edit the bundle composer to use the local graphqlite
 $composerBundle = json_decode(file_get_contents($composerBundlePath), true);
 
@@ -18,6 +16,6 @@ $composerBundle['repositories'] = [
     ]
 ];
 
-$composerBundle['require']['thecodingmachine/graphqlite'] = $masterAlias;
+$composerBundle['require']['thecodingmachine/graphqlite'] = '*';
 
 file_put_contents($composerBundlePath, json_encode($composerBundle));
