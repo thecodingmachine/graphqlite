2bb53bb8f2d7b716a5787e88d8b9c53a91ccb29f
Jacob Thomason <jacob@thomason.xxx>
2021-08-31 04:54:10 -0400

    Fix node target issue

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -2,8 +2,10 @@ import Layout from "@theme/Layout";
 import React from "react";
 
 export default () => {
-  window.location.href = "/docs";
-
+  if (typeof window !== "undefined") {
+      window.location.href = "/docs";
+  }
+  
   // Rendering the Layout helps keep the page from jumping, it takes a minute for the
   // location to change
   return <Layout />;
