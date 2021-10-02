7e6350adef0c42de5fca4f0f4172d763a31a3916
Daniel Melchior <daniel.melchior@interweber.de>
2021-10-02 13:02:48 +0200

    fix: ensure that extended fields in MutableObjectType instances are found

--- src/Types/MutableObjectType.php
+++ src/Types/MutableObjectType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace TheCodingMachine\GraphQLite\Types;
 
+use GraphQL\Type\Definition\FieldDefinition;
 use GraphQL\Type\Definition\ObjectType;
 
 /**
@@ -23,5 +24,20 @@ class MutableObjectType extends ObjectType implements MutableInterface
 
         parent::__construct($config);
         $this->className = $className;
+    }
+
+    public function findField(string $name): ?FieldDefinition
+    {
+        $field = parent::findField($name);
+        if ($field) {
+            return $field;
+        }
+
+        $fields = $this->getFields();
+        if (! isset($fields[$name])) {
+            return null;
+        }
+
+        return $fields[$name];
     }
 }
