4daecddf9bea310527a89c745c17f7ef4aee93cc
madness <ss@gmail.com>
2022-08-30 23:03:58 +0300

    Bad params with stdClass test removed. It's already throws an error in php 8.0

--- tests/Annotations/SecurityTest.php
+++ tests/Annotations/SecurityTest.php
@@ -23,11 +23,4 @@ class SecurityTest extends TestCase
         $this->expectExceptionMessage('A @Security annotation that has "failWith" attribute set cannot have a message or a statusCode attribute.');
         new Security(['expression'=>'foo', 'failWith'=>null, 'statusCode'=>500]);
     }
-
-    public function testBadParams2(): void
-    {
-        $this->expectException(TypeError::class);
-        $this->expectExceptionMessage('"TheCodingMachine\GraphQLite\Annotations\Security::__construct": Argument $data is expected to be a string or array, got "object".');
-        new Security(new stdClass());
-    }
 }
