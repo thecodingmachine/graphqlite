6f92ac8e33841077426fca373e4434f712422483
bladl <vladgadzyra@gmail.com>
2023-03-13 09:00:12 +0200

    Bump doctrine/annotations to v2

--- tests/Fixtures/Annotations/ClassWithInvalidExtendTypeAnnotation.php
+++ tests/Fixtures/Annotations/ClassWithInvalidExtendTypeAnnotation.php
@@ -5,9 +5,8 @@ namespace TheCodingMachine\GraphQLite\Fixtures\Annotations;
 
 use TheCodingMachine\GraphQLite\Annotations\ExtendType;
 
-/**
- * @ExtendType(class="foo")
- */
+
+#[ExtendType(class: 'foo')]
 class ClassWithInvalidExtendTypeAnnotation
 {
 }
