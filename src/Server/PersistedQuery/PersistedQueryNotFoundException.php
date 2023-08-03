1c1d37a11fe1b336fc802e364e948875d86231ba
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2023-08-03 16:50:45 +0300

    Make persisted query errors at least extend GraphQL errors so they are treated as such

--- src/Server/PersistedQuery/PersistedQueryNotFoundException.php
+++ src/Server/PersistedQuery/PersistedQueryNotFoundException.php
@@ -2,12 +2,13 @@
 
 namespace TheCodingMachine\GraphQLite\Server\PersistedQuery;
 
+use GraphQL\Error\Error;
 use Throwable;
 
 /**
  * See https://github.com/apollographql/apollo-client/blob/fc450f227522c5311375a6b59ec767ac45f151c7/src/link/persisted-queries/index.ts#L73
  */
-class PersistedQueryNotFoundException extends \RuntimeException implements PersistedQueryException
+class PersistedQueryNotFoundException extends Error implements PersistedQueryException
 {
     public function __construct(Throwable $previous = null) {
         parent::__construct('Persisted query by that ID was not found and "query" was omitted.', previous: $previous);
