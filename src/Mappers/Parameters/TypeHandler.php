f2d8895bc68cff3a01b38c5f8c47fe2eef078d92
Jacob Thomason <jacob@thomason.xxx>
2023-10-15 19:43:20 -0400

    Corrected some logic and improved for durability

--- src/Mappers/Parameters/TypeHandler.php
+++ src/Mappers/Parameters/TypeHandler.php
@@ -126,22 +126,35 @@ class TypeHandler implements ParameterHandlerInterface
         $varTags = $docBlock->getTagsByName('var');
 
         if (! $varTags) {
-            return null;
-        }
-
-        if (count($varTags) > 1) {
             // If we don't have any @var tags, was this property promoted, and if so, do we have an
             // @param tag on the constructor docblock?  If so, use that for the type.
             if ($refProperty->isPromoted()) {
-                $docBlock = $this->cachedDocBlockFactory->getDocBlock($refProperty->getDeclaringClass()->getConstructor());
+                $refConstructor = $refProperty->getDeclaringClass()->getConstructor();
+                if (! $refConstructor) {
+                    return null;
+                }
+
+                $docBlock = $this->cachedDocBlockFactory->getDocBlock($refConstructor);
                 $paramTags = $docBlock->getTagsByName('param');
                 foreach ($paramTags as $paramTag) {
+                    if (! method_exists($paramTag, 'getVariableName')) {
+                        continue;
+                    }
+
+                    if (! method_exists($paramTag, 'getType')) {
+                        continue;
+                    }
+
                     if ($paramTag->getVariableName() === $refProperty->getName()) {
                         return $paramTag->getType();
                     }
                 }
             }
-            
+
+            return null;
+        }
+
+        if (count($varTags) > 1) {
             throw InvalidDocBlockRuntimeException::tooManyVarTags($refProperty);
         }
 
