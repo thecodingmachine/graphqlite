c5da1f08a74d6b6579de6ab2778d715761c3f8de
grynchuk <greenchuckaa@gmail.com>
2024-03-08 15:25:02 +0200

    Fix cs

--- src/PrefetchBuffer.php
+++ src/PrefetchBuffer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace TheCodingMachine\GraphQLite;
 
 use GraphQL\Type\Definition\ResolveInfo;
+
 use function array_key_exists;
 use function md5;
 use function serialize;
@@ -24,7 +25,7 @@ class PrefetchBuffer
     public function register(
         object $object,
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): void {
         $this->objects[$this->computeHash($arguments, $info)][] = $object;
     }
@@ -32,16 +33,17 @@ class PrefetchBuffer
     /** @param array<array-key, mixed> $arguments The input arguments passed from GraphQL to the field. */
     private function computeHash(
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): string {
         if (
-            null === $info
-            || false === isset($info->operation)
-            || null === ($queryBody = $info->operation->loc?->source?->body)
+            $info instanceof ResolveInfo
+            && isset($info->operation)
+            && $info->operation->loc?->source?->body !== null
         ) {
-            return md5(serialize($arguments));
+            return md5(serialize($arguments) . $info->operation->loc?->source?->body);
         }
-        return md5(serialize($arguments) . $queryBody);
+
+        return md5(serialize($arguments));
     }
 
     /**
@@ -51,7 +53,7 @@ class PrefetchBuffer
      */
     public function getObjectsByArguments(
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): array {
         return $this->objects[$this->computeHash($arguments, $info)] ?? [];
     }
@@ -59,7 +61,7 @@ class PrefetchBuffer
     /** @param array<array-key, mixed> $arguments The input arguments passed from GraphQL to the field. */
     public function purge(
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): void {
         unset($this->objects[$this->computeHash($arguments, $info)]);
     }
@@ -68,7 +70,7 @@ class PrefetchBuffer
     public function storeResult(
         mixed $result,
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): void {
         $this->results[$this->computeHash($arguments, $info)] = $result;
     }
@@ -76,7 +78,7 @@ class PrefetchBuffer
     /** @param array<array-key, mixed> $arguments The input arguments passed from GraphQL to the field. */
     public function hasResult(
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): bool {
         return array_key_exists($this->computeHash($arguments, $info), $this->results);
     }
@@ -84,7 +86,7 @@ class PrefetchBuffer
     /** @param array<array-key, mixed> $arguments The input arguments passed from GraphQL to the field. */
     public function getResult(
         array $arguments,
-        ?ResolveInfo $info = null
+        mixed $info = null,
     ): mixed {
         return $this->results[$this->computeHash($arguments, $info)];
     }
