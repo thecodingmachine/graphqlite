b3a9186314e5b43c554a666978d59b7c6ec2fdb9
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-03-29 16:31:43 +0200

    Add test for promoted default value

--- tests/Integration/EndToEndTest.php
+++ tests/Integration/EndToEndTest.php
@@ -1911,6 +1911,7 @@ class EndToEndTest extends IntegrationTestCase
                 magazine: "Test"
             }) {
                 magazine
+                summary
             }
         }
         ';
@@ -1922,6 +1923,7 @@ class EndToEndTest extends IntegrationTestCase
 
         $data = $this->getSuccessResult($result);
         $this->assertSame('Test', $data['updateArticle']['magazine']);
+        $this->assertSame('default', $data['updateArticle']['summary']);
         $queryString = '
         mutation {
             updateArticle(input: {
