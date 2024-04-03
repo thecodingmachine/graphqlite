e3d5d1a066d279174984a7930d635c70e47aba6c
Shish <shish@shishnet.org>
2024-04-04 00:19:58 +0100

    Add "no framework" example to examples folder
    Having an example as actual code rather than as out-of-context snippets inside a text file means that we can run the code, and ensure that it actually works
    
    Submitting mostly for discussion to see if people like this idea
    
    TODO:
    - have a github action which runs the code and ensures that the "hello world" query works (thus ensuring that the example code is correct)
    - install graphqlite from parent folder instead of downloading the published 6.X (thus ensuring that the example is always in-sync with the library it is an example of)
    - update the documentation to reference the files in this repository, rather than embedding hard-coded out-of-context snippets into the .mdx files (thus ensuring that the documentation stays in-sync with the known-good example code)

---
+++ examples/no-framework/index.php
@@ -0,0 +1,41 @@
+<?php
+use GraphQL\GraphQL;
+use GraphQL\Type\Schema;
+use TheCodingMachine\GraphQLite\SchemaFactory;
+use TheCodingMachine\GraphQLite\Context\Context;
+
+use Symfony\Component\Cache\Simple\FilesystemCache;
+use Mouf\Picotainer\Picotainer;
+use GraphQL\Utils\SchemaPrinter;
+use App\Controllers\MyController;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// $cache is any PSR-16 compatible cache.
+$cache = new FilesystemCache();
+
+// $container is any PSR-11 compatible container which has
+// been populated with your controller classes.
+$container = new Picotainer([
+        MyController::class => function() {
+                return new MyController();
+        },
+]);
+
+$factory = new SchemaFactory($cache, $container);
+$factory->addControllerNamespace('App\\Controllers')
+        ->addTypeNamespace('App');
+
+$schema = $factory->createSchema();
+
+$rawInput = file_get_contents('php://input');
+$input = json_decode($rawInput, true);
+$query = $input['query'];
+$variableValues = isset($input['variables']) ? $input['variables'] : null;
+
+$result = GraphQL::executeQuery($schema, $query, null, new Context(), $variableValues);
+$output = $result->toArray();
+
+header('Content-Type: application/json');
+echo json_encode($output) . "\n";
+
