7a75a73eb29ee3837cb935c3f05f68cfb37f1fba
Jacob Thomason <jacob@thomason.xxx>
2024-12-18 02:26:29 -0500

    Fixed Adapter class for compatibility with base ObjectType (#721)

--- src/Mappers/Proxys/MutableObjectTypeAdapter.php
+++ src/Mappers/Proxys/MutableObjectTypeAdapter.php
@@ -3,23 +3,11 @@
 
 namespace TheCodingMachine\GraphQLite\Mappers\Proxys;
 
-use Exception;
-use GraphQL\Error\InvariantViolation;
-use GraphQL\Type\Definition\FieldDefinition;
 use GraphQL\Type\Definition\InterfaceType;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\ResolveInfo;
-use GraphQL\Utils\Utils;
-use RuntimeException;
-use TheCodingMachine\GraphQLite\Types\MutableInterface;
 use TheCodingMachine\GraphQLite\Types\MutableObjectType;
-use TheCodingMachine\GraphQLite\Types\NoFieldsException;
 use function assert;
-use function call_user_func;
-use function is_array;
-use function is_callable;
-use function is_string;
-use function sprintf;
 
 /**
  * An adapter class (actually a proxy) that adds the "mutable" feature to any Webonyx ObjectType.
@@ -36,6 +24,7 @@ final class MutableObjectTypeAdapter extends MutableObjectType
         $this->type = $type;
         $this->className = $className;
         $this->name = $type->name;
+        $this->description = $type->description;
         $this->config = $type->config;
         $this->astNode = $type->astNode;
         $this->extensionASTNodes = $type->extensionASTNodes;
