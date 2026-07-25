f04ddb569d7b77cbdeb4ef4e112be84a4f9b8cce
Michael Georgiadis <38563912+michael-georgiadis@users.noreply.github.com>
2026-07-25 03:17:58 +0200

    Update test to sane docblock

--- tests/Fixtures/Integration/Models/UpdateArticleInput.php
+++ tests/Fixtures/Integration/Models/UpdateArticleInput.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TheCodingMachine\GraphQLite\Fixtures\Integration\Models;
 
 use TheCodingMachine\GraphQLite\Annotations\Field;
@@ -10,15 +12,15 @@ use TheCodingMachine\GraphQLite\Undefined;
 #[Input]
 class UpdateArticleInput
 {
-    /** @param list<string>|null $tags */
+    /** @param list<string>|Undefined|null $tags */
     public function __construct(
         #[Field]
         #[Security("magazine != 'NYTimes'")]
-        public readonly string|null|Undefined $magazine = Undefined::VALUE,
+        public readonly string|Undefined|null $magazine = Undefined::VALUE,
         #[Field]
         public readonly string $summary = 'default',
         #[Field]
-        public readonly array|null|Undefined $tags = Undefined::VALUE,
+        public readonly array|Undefined|null $tags = Undefined::VALUE,
     )
     {
     }
